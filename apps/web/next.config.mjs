512b117a2dc0dc399d56c8503eacf9ceb42a0461
Lucas Doell <lucas@lux.dev>
2024-09-05 18:33:01 -0400

    Add rewrite rule for API endpoint

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/v1/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
