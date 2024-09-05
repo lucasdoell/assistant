c58f87fd1fdbfcb91a04aee268f5719902f9174d
Lucas Doell <lucas@lux.dev>
2024-09-05 18:34:01 -0400

    Update API rewrite rule to use generic path

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/v1/:path*`,
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
