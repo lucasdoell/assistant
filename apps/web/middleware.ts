b4ca85f8748149eb3b519b635aa07dcd9e0da076
Lucas Doell <lucas@lux.dev>
2024-09-05 21:32:25 -0400

    Refactor nextjs middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -6,18 +6,12 @@ const AUTH_ROUTES = ["/login", "/signup"];
 export async function middleware(request: NextRequest) {
   const cookie = request.cookies.get("luxAuth")?.value;
 
-  const isAuthPage = AUTH_ROUTES.includes(request.nextUrl.pathname);
-
-  if (isAuthPage) {
+  if (AUTH_ROUTES.includes(request.nextUrl.pathname)) {
     if (cookie) {
       return NextResponse.redirect(new URL("/dashboard", request.url));
     }
 
     return null;
-  }
-
-  if (!cookie) {
-    return NextResponse.redirect(new URL("/login", request.url));
   }
 }
 
