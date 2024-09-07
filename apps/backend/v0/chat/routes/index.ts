4302be88f282a4367f410119c3571b05f85d7c14
Lucas Doell <lucas@lux.dev>
2024-09-07 02:30:13 -0400

    Delete v0 routes

--- apps/backend/v0/chat/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { chatController } from "@/v1/chat/controllers/chat";
-import { authenticate } from "@/v1/chat/middlewares/auth";
-import express from "express";
-
-const chatRouter = express.Router();
-
-chatRouter.post("/", authenticate(), chatController);
-
-export { chatRouter };
