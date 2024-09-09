cbb0a49ec7cf5c1079134b989d7f10b2ab53d21c
Lucas Doell <lucas@lux.dev>
2024-09-09 02:19:57 -0400

    Add Chat component for chat functionality

---
+++ apps/web/components/chat/chat.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+import { Message } from "@/components/chat/message";
+import { ScrollArea } from "@ui/scroll-area";
+import { useChat } from "ai/react";
+
+export function Chat() {
+  const { messages, input, handleInputChange, handleSubmit, data } = useChat({
+    api: "http://localhost:8080/v1/chat",
+  });
+
+  return (
+    <>
+      {/* Chat Header */}
+      <ChatHeader />
+
+      {/* Messages */}
+      <ScrollArea className="flex-1 p-4 pb-20">
+        {messages.map((m) => (
+          <Message key={m.id} message={m} />
+        ))}
+      </ScrollArea>
+
+      {/* Floating Message Input */}
+      <ChatInput
+        handleSubmit={handleSubmit}
+        handleInputChange={handleInputChange}
+        input={input}
+      />
+    </>
+  );
+}
