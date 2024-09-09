58774525423106b4963ea31e176165d65e00cfde
Lucas Doell <lucas@lux.dev>
2024-09-09 01:21:03 -0400

    Add TooltipProvider to RootLayout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,6 +4,7 @@ import { CommandDialogMenu } from "@/components/cmd-menu";
 import { CommandMenuProvider } from "@/components/cmd-provider";
 import { QueryProvider } from "@/components/query-provider";
 import { ThemeProvider } from "@/components/theme-provider";
+import { TooltipProvider } from "@ui/tooltip";
 import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 
@@ -28,8 +29,10 @@ export default function RootLayout({
         >
           <QueryProvider>
             <CommandMenuProvider>
-              <CommandDialogMenu />
-              {children}
+              <TooltipProvider>
+                <CommandDialogMenu />
+                {children}
+              </TooltipProvider>
             </CommandMenuProvider>
           </QueryProvider>
         </ThemeProvider>
