d50149e7c3971f377ece9e4083dfb908653f4baf
Lucas Doell <lucas@lux.dev>
2024-09-11 19:19:52 -0400

    Update Chat component to handle existing messages and redirect to chat page

--- apps/web/components/chat/chat.tsx
+++ apps/web/components/chat/chat.tsx
@@ -4,12 +4,29 @@ import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
 import { Message } from "@/components/chat/message";
 import { ScrollArea } from "@ui/scroll-area";
+import type { Message as ChatMessage } from "ai";
 import { useChat } from "ai/react";
+import { useEffect } from "react";
 
-export function Chat() {
+export function Chat({
+  existingMessages,
+  chatId,
+}: {
+  existingMessages?: ChatMessage[];
+  chatId?: string;
+}) {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat({
-    api: "http://localhost:8080/v1/chat",
+    api: `http://localhost:8080/v1/chat${existingMessages ? "/" + chatId : ""}`,
+    initialMessages: existingMessages ?? [],
+    credentials: "include",
   });
+
+  useEffect(() => {
+    if (data && data[0] && !existingMessages) {
+      const { chatId } = data[0] as { chatId: string };
+      window.location.href = `/chat/${chatId}`;
+    }
+  }, [data]);
 
   return (
     <>
@@ -18,6 +35,8 @@ export function Chat() {
 
       {/* Messages */}
       <ScrollArea className="flex-1 p-4 pb-20 2xl:max-w-4xl 2xl:mx-auto">
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+
         {messages.map((m) => (
           <Message key={m.id} message={m} />
         ))}
